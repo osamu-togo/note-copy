e782b8c06bee9756c9c3e663253f6b62a1be1da7
osamu-togo <ku_ri_ka_e_su@i.softbank.jp>
2020-07-21 23:49:43 +0900

    like modelを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_122409) do
+ActiveRecord::Schema.define(version: 2020_07_21_144704) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image"
+    t.integer "note_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
     t.integer "note_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
